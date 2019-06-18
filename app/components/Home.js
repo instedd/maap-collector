5ada806c92fdd013d99c02830d05c70a5f949e9a
pmallol <pmallol@manas.tech>
2019-06-18 16:26:51 -0300

    First commit

---
+++ app/components/Home.js
@@ -0,0 +1,20 @@
+// @flow
+/* eslint no-plusplus: off */
+import React, { Component } from 'react';
+
+type Props = {};
+type State = {};
+
+export default class Home extends Component<Props, State> {
+  props: Props;
+
+  state: State = {};
+
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
---
+++ test/e2e/HomePage.e2e.js
@@ -0,0 +1,22 @@
+import { ClientFunction } from 'testcafe';
+
+const getPageTitle = ClientFunction(() => document.title);
+const assertNoConsoleErrors = async t => {
+  const { error } = await t.getBrowserConsoleMessages();
+  await t.expect(error).eql([]);
+};
+
+fixture`Home Page`.page('../../app/app.html').afterEach(assertNoConsoleErrors);
+
+test('e2e', async t => {
+  await t.expect(getPageTitle()).eql('Hello Electron React!');
+});
+
+test('should open window', async t => {
+  await t.expect(getPageTitle()).eql('Hello Electron React!');
+});
+
+test(
+  "should haven't any logs in console of main window",
+  assertNoConsoleErrors
+);
