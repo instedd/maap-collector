6b71b2f3ae2d3633e0d51f5a234a1391dbec1495
pmallol <pmallol@manas.tech>
2019-06-21 12:51:49 -0300

    [WIP] Login form

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,19 +1,76 @@
 // @flow
 /* eslint no-plusplus: off */
 import React, { Component } from 'react';
+import Button from '@material/react-button';
+import Card, { CardPrimaryContent, CardActions, CardActionButtons } from '@material/react-card';
+import {Cell, Grid, Row} from '@material/react-layout-grid';
+import TextField, { Input } from '@material/react-text-field';
+import styles from './Home.css';
 
 type Props = {};
-type State = {};
+type State = {
+  username: string,
+  password: string
+};
 
 export default class Home extends Component<Props, State> {
   props: Props;
 
-  state: State = {};
+  state: State = {
+    username: "",
+    password: ""
+  };
+
+  handleSubmit(event) {
+    console.log('Submit');
+  }
 
   render() {
     return (
       <div>
-        <h2>Home</h2>
+        <Grid>
+          <Row>
+            <h2>Home</h2>
+          </Row>
+          <Row>
+            <Cell columns={4} align="middle">
+            <Card>
+              <Grid>
+                <form onSubmit={this.handleSubmit}>
+                  <Row>
+                    <Cell columns={12}>
+                      <TextField
+                        label='Username'
+                      ><Input
+                        value={this.state.username}
+                        onChange={(e) => this.setState({username: e.currentTarget.value})} />
+                      </TextField>
+                    </Cell>
+                    <Cell columns={12}>
+                      <TextField
+                        label='Password'
+                      ><Input
+                        value={this.state.password}
+                        onChange={(e) => this.setState({password: e.currentTarget.value})} />
+                      </TextField>
+                    </Cell>
+                  </Row>
+                  <CardActions>
+                    <CardActionButtons>
+                      <Button
+                      raised
+                      onClick={() => console.log(this.state.username)}
+                      >
+                        Login
+                      </Button>
+                    </CardActionButtons>
+                  </CardActions>
+                </form>
+                </Grid>
+              </Card>
+            </Cell>
+          </Row>
+        </Grid>
       </div>
     );
   }
