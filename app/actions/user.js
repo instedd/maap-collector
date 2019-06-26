b58a2d15a6efb4df11c9cae326f53f06f558982c
pmallol <pmallol@manas.tech>
2019-06-26 18:53:53 -0300

    [WIP] Move Login form to its own component with added functionality

--- app/actions/user.js
+++ app/actions/user.js
@@ -2,14 +2,44 @@ import db from '../db';
 
 const USER_LOGGED_IN = 'USER_LOGGED_IN';
 const USER_LOGGED_OUT = 'USER_LOGGED_OUT';
+const REQUEST_LOGIN = 'REQUEST_LOGIN';
+const USER_LOGGED_IN_FAILURE = 'USER_LOGGED_IN_FAILURE';
 
-export const userLoggedIn = (user = { id: 7, password: '123' }) => {
-  db.initialize(user.id, user.password);
-  return { type: 'USER_LOGGED_IN', user };
-};
+export const requestLogin = (username, password) => (dispatch) => {
+  dispatch({ type: 'REQUEST_LOGIN' })
+  fetch('http://localhost:3000/api/v1/auth/sign_in', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'email': username,
+      'password': password
+    })
+  })
+  .then(response => {
+    if(response.status === 200) return response.json()
+    return Promise.reject(new Error ('Unauthorized'))
+  })
+  .then(response => dispatch(userLoggedIn({ username, password, response })))
+  .catch(() => dispatch(userLoggedInFailure()))
+}
 
-export const userLoggedOut = (user = { id: 7, password: '123' }) =>
+export const userLoggedIn = (user) => {
+  db.initialize('dbname', user.password)
+  return { type: 'USER_LOGGED_IN', user }
+}
+
+export const userLoggedInFailure = () => ({ type: 'USER_LOGGED_IN_FAILURE' })
+
+export const userLoggedOut = (user) => {
   // TODO: Decide what to do with the old database
-  ({ type: 'USER_LOGGED_OUT', user });
+  ({ type: 'USER_LOGGED_OUT', user })
+}
 
-export { USER_LOGGED_IN, USER_LOGGED_OUT };
+export {
+  USER_LOGGED_IN,
+  USER_LOGGED_OUT,
+  REQUEST_LOGIN,
+  USER_LOGGED_IN_FAILURE
+};
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,76 +1,20 @@
 // @flow
 /* eslint no-plusplus: off */
 import React, { Component } from 'react';
-import Button from '@material/react-button';
-import Card, { CardPrimaryContent, CardActions, CardActionButtons } from '@material/react-card';
-import {Cell, Grid, Row} from '@material/react-layout-grid';
-import TextField, { Input } from '@material/react-text-field';
 import styles from './Home.css';
 
 type Props = {};
-type State = {
-  username: string,
-  password: string
-};
+type State = {};
 
 export default class Home extends Component<Props, State> {
   props: Props;
 
-  state: State = {
-    username: "",
-    password: ""
-  };
-
-  handleSubmit(event) {
-    console.log('Submit');
-  }
+  state: State = {};
 
   render() {
     return (
       <div>
-        <Grid>
-          <Row>
-            <h2>Home</h2>
-          </Row>
-          <Row>
-            <Cell columns={4} align="middle">
-            <Card>
-              <Grid>
-                <form onSubmit={this.handleSubmit}>
-                  <Row>
-                    <Cell columns={12}>
-                      <TextField
-                        label='Username'
-                      ><Input
-                        value={this.state.username}
-                        onChange={(e) => this.setState({username: e.currentTarget.value})} />
-                      </TextField>
-                    </Cell>
-                    <Cell columns={12}>
-                      <TextField
-                        label='Password'
-                      ><Input
-                        value={this.state.password}
-                        onChange={(e) => this.setState({password: e.currentTarget.value})} />
-                      </TextField>
-                    </Cell>
-                  </Row>
-                  <CardActions>
-                    <CardActionButtons>
-                      <Button
-                      raised
-                      onClick={() => console.log(this.state.username)}
-                      >
-                        Login
-                      </Button>
-                    </CardActionButtons>
-                  </CardActions>
-                </form>
-                </Grid>
-              </Card>
-            </Cell>
-          </Row>
-        </Grid>
+        <h2>Home</h2>
       </div>
     );
   }
