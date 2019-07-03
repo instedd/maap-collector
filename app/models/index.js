598e98ee44f5ea98b81479a80626c81a61409d44
hdf1986 <hugo.farji+github@gmail.com>
2019-07-03 16:04:37 -0300

    Fix for e2e tests

--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,5 @@
-import fs from 'fs';
+import Lab from './lab';
 
-const path = require('path');
-
-const basename = path.basename(__filename);
-
-export default fs
-  .readdirSync(path.join(__dirname, 'models'))
-  .filter(
-    file =>
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file !== 'index.js'
-  )
-  .reduce((acc, file) => {
-    // eslint-disable-next-line
-    const req = require(`./${file}`).default;
-    return { [req.modelName]: req, ...acc };
-  }, {});
+export default {
+  Lab
+};
