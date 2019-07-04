79d3f3f4c864c41f0ba1093b256dec0f5fedc03e
pmallol <pmallol@manas.tech>
2019-07-04 18:49:17 -0300

    Change app title

--- test/e2e/HomePage.e2e.js
+++ test/e2e/HomePage.e2e.js
@@ -9,11 +9,11 @@ const assertNoConsoleErrors = async t => {
 fixture`Home Page`.page('../../app/app.html').afterEach(assertNoConsoleErrors);
 
 test('e2e', async t => {
-  await t.expect(getPageTitle()).eql('Hello Electron React!');
+  await t.expect(getPageTitle()).eql('MAAP Data Collector');
 });
 
 test('should open window', async t => {
-  await t.expect(getPageTitle()).eql('Hello Electron React!');
+  await t.expect(getPageTitle()).eql('MAAP Data Collector');
 });
 
 test(
