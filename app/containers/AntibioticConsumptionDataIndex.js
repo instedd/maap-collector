47405a6d90ea6845f73435b5b418fe8c03ca7a8a
hdf1986 <hugo.farji+github@gmail.com>
2019-07-11 12:01:12 -0300

    Added fab button

--- app/containers/AntibioticConsumptionDataIndex.js
+++ app/containers/AntibioticConsumptionDataIndex.js
@@ -1,6 +1,9 @@
 // @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import style from '@material/react-fab/dist/fab.css';
+import { Fab } from '@material/react-fab';
+import MaterialIcon from '@material/react-material-icon';
 import AntibioticConsumptionStatsList from '../components/AntibioticConsumptionStatsList';
 
 type Props = {
@@ -18,6 +21,11 @@ class AntibioticConsumptionStatsIndex extends Component<Props> {
     return (
       <div>
         <AntibioticConsumptionStatsList />
+        <Fab
+          className={[style['mdc-fab'], 'app-fab--absolute']}
+          icon={<MaterialIcon icon="add" />}
+          onClick={() => console.log('test')}
+        />
       </div>
     );
   }
