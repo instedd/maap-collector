2b96357bfefca218e21294be4f6bc3f6f5425ee0
hdf1986 <hugo.farji+github@gmail.com>
2019-07-25 13:28:42 -0300

    Fixed local storage persistence

--- app/utils/localStorage.js
+++ app/utils/localStorage.js
@@ -1,6 +1,10 @@
+const Store = require('electron-store');
+
+const store = new Store();
+
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem('state');
+    const serializedState = store.get('state');
     if (serializedState === null) {
       return undefined;
     }
@@ -13,7 +17,7 @@ export const loadState = () => {
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
+    store.set('state', serializedState);
   } catch {
     // ignore write errors
   }
