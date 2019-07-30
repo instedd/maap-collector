948400e78d5c07f93b4f21051355220a0c660adf
hdf1986 <hugo.farji+github@gmail.com>
2019-07-30 17:33:53 -0300

    Fixed font on production mode

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import { AppContainer } from 'react-hot-loader';
 import { throttle } from 'lodash';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import './app.global.scss';
 
 import { setNetworkOnline, setNetworkOffline } from './actions/network';
