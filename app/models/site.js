fa1ed7127f278851f715810ee8316bd2df4a9cdf
Martin Verzilli <mverzilli@manas.tech>
2019-08-09 12:17:20 -0300

    Add level and teaching fields to Sites

--- app/models/site.js
+++ app/models/site.js
@@ -13,7 +13,9 @@ const model = sequelize =>
       identifiedPatients: { type: Sequelize.BOOLEAN },
       permanentlyIdentifiedPatients: { type: Sequelize.BOOLEAN },
       lastSyncAt: { type: Sequelize.DATE },
-      remoteId: { type: Sequelize.INTEGER }
+      remoteId: { type: Sequelize.INTEGER },
+      level: { type: Sequelize.STRING },
+      teaching: { type: Sequelize.STRING }
     },
     { sequelize, modelName: model.modelName }
   );
