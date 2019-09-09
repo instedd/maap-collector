a56c8e86dfe1f4365e5829d9c8156c34905bc3a6
hdf1986 <hugo.farji+github@gmail.com>
2019-09-09 13:20:26 -0300

    Minor fixes for lab records import

--- app/reducers/labRecordImport.js
+++ app/reducers/labRecordImport.js
@@ -1,5 +1,8 @@
 import {
   SET_IMPORT_DATA,
+  SET_PHI_DATA,
+  SET_PATIENT_ID_DATA,
+  SET_FILE_DATA,
   CLEAN_LAB_RECORD_IMPORT
 } from '../actions/labRecordImport';
 import type { Action } from './types';
@@ -18,6 +21,12 @@ const initialState = {
 
 export default function counter(state = initialState, action: Action) {
   switch (action.type) {
+    case SET_PHI_DATA:
+      return { ...state, ...action.data };
+    case SET_PATIENT_ID_DATA:
+      return { ...state, ...action.data };
+    case SET_FILE_DATA:
+      return { ...state, ...action.data };
     case SET_IMPORT_DATA:
       return { ...state, ...action.data };
     case CLEAN_LAB_RECORD_IMPORT:
