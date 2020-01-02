094c29627d6f8e3aa5c982afaa75942713e8d418
hdf1986 <hugo.farji+github@gmail.com>
2020-01-02 15:59:10 -0300

    Added new build process

---
+++ release.js
@@ -0,0 +1,45 @@
+const Octokit = require('@octokit/rest');
+const fs = require('fs');
+const { version } = require('./package.json');
+const environments = require('./environments');
+
+const { GITHUB_TOKEN } = process.env;
+const octokit = Octokit({
+  auth: GITHUB_TOKEN,
+  userAgent: 'Maap collector release tool'
+});
+
+console.log('Releasing Maap collector');
+console.log(`Version ${version}`);
+
+const versionNameTag = (versionNumber, name) => `${versionNumber}-${name}`;
+octokit.repos
+  .listReleases({
+    owner: 'instedd',
+    repo: 'maap-collector'
+  })
+  .then(data => {
+    const { data: releases } = data;
+    environments.forEach(environment => {
+      const versionName = versionNameTag(version, environment.name);
+      if (!releases.map(({ name }) => name).includes(versionName)) {
+        octokit.repos.createRelease({
+          owner: 'instedd',
+          repo: 'maap-collector',
+          tag_name: `V${versionName}`,
+          draft: true,
+          name: versionName
+        });
+      }
+      console.log(`Releasing ${versionName} with config:`);
+      console.log(environment.config);
+
+      fs.writeFileSync(
+        './app/constants/config.override.json',
+        JSON.stringify(environment.config),
+        () => {}
+      );
+    });
+    return data;
+  })
+  .catch(err => console.log(err));
