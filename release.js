75caa55bb6d24d605ac3341c01a2824c7184229b
hdf1986 <hugo.farji+github@gmail.com>
2020-01-06 08:55:04 -0300

    Updating travis config-

--- release.js
+++ release.js
@@ -13,58 +13,52 @@ const octokit = Octokit({
 console.log('Releasing Maap collector');
 console.log(`Version ${version}`);
 
-const asyncForEach = async (array, callback) => {
-  for (let index = 0; index < array.length; index += 1) {
-    // eslint-disable-next-line
-    await callback(array[index], index, array);
-  }
-};
-
 const versionNameTag = (versionNumber, name) => `${versionNumber}-${name}`;
 octokit.repos
   .listReleases({
     owner: 'instedd',
     repo: 'maap-collector'
   })
-  .then(data => {
+  .then(async data => {
     const { data: releases } = data;
-    asyncForEach(environments, async environment => {
-      const versionName = versionNameTag(version, environment.name);
-      if (!releases.map(({ name }) => name).includes(versionName)) {
-        octokit.repos.createRelease({
-          owner: 'instedd',
-          repo: 'maap-collector',
-          tag_name: `V${versionName}`,
-          draft: true,
-          name: versionName
-        });
-      }
-      console.log(`Releasing ${versionName} with config:`);
-      console.log(environment.config);
+    const environment = environments.find(k => k.name === process.env.INSTANCE);
+    if (!process.env.INSTANCE)
+      return console.log('INSTANCE env var is missing');
+    const versionName = versionNameTag(version, environment.name);
+    if (!releases.map(({ name }) => name).includes(versionName)) {
+      octokit.repos.createRelease({
+        owner: 'instedd',
+        repo: 'maap-collector',
+        tag_name: `V${versionName}`,
+        draft: true,
+        name: versionName
+      });
+    }
+    console.log(`Releasing ${versionName} with config:`);
+    console.log(environment.config);
 
-      fs.writeFileSync(
-        './app/constants/config.override.json',
-        JSON.stringify(environment.config)
-      );
-      // eslint-disable-next-line
-      const packageJson = require('./package.json');
-      packageJson.version = versionName;
-      fs.writeFileSync('./package.json', JSON.stringify(packageJson));
-      await new Promise(resolve => {
-        const ls = spawn('yarn', ['package-ci']);
-        ls.stdout.on('data', output => {
-          console.log(`stdout: ${output.toString()}`);
-        });
+    fs.writeFileSync(
+      './app/constants/config.override.json',
+      JSON.stringify(environment.config)
+    );
+    // eslint-disable-next-line
+    const packageJson = require('./package.json');
+    packageJson.version = versionName;
+    fs.writeFileSync('./package.json', JSON.stringify(packageJson));
+    await new Promise(resolve => {
+      const ls = spawn('yarn', ['package-ci']);
+      ls.stdout.on('data', output => {
+        console.log(`stdout: ${output.toString()}`);
+      });
 
-        ls.stderr.on('data', output => {
-          resolve(output);
-          console.log(`stderr: ${output.toString()}`);
-        });
+      ls.stderr.on('data', output => {
+        resolve(output);
+        console.log(`stderr: ${output.toString()}`);
+      });
 
-        ls.on('exit', output => {
-          resolve(output);
-          console.log(`child process exited with code ${output.toString()}`);
-        });
+      ls.on('exit', output => {
+        resolve(output);
+        console.log(`child process exited with code ${output.toString()}`);
       });
     });
     return data;
