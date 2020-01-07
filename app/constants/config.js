158ab10971c89526c7d814dc2674857b332e39a7
Hugo David Farji <hfarji@manas.com.ar>
2020-01-07 10:57:47 -0300

    Release improvement (#240)
    * Added new build process
    
    * Fixed typo in branch name for releases
    
    * Added write to package json
    
    * Added release script
    
    * Added release script
    
    * Added release output to script
    
    * Commented tests
    
    * Updating travis config-
    
    * Updating travis config-
    
    * Updating travis config-
    
    * Updating travis config
    
    * Patch for windows
    
    * Fixed missing yarn_gpg no
    
    * Added stages
    
    * Added new builds
    
    * Added extra info on deployment

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,8 +1,13 @@
+const override = require('./config.override.json');
+
 require('dotenv').config();
 
 const {
   API_URL = 'https://maap-stg.instedd.org',
   ELECTRON_ENV = 'development'
-} = process.env;
+} = {
+  ...process.env,
+  ...override
+};
 
 export { API_URL, ELECTRON_ENV };
---
+++ environments.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: 'staging',
+    config: {
+      API_URL: 'https://maap-stg.instedd.org'
+    }
+  },
+  {
+    name: 'demo',
+    config: {
+      API_URL: 'https://maap-demo.instedd.org'
+    }
+  },
+  {
+    name: 'bf',
+    config: {
+      API_URL: 'https://maap-bf.instedd.org'
+    }
+  },
+  {
+    name: 'zw',
+    config: {
+      API_URL: 'https://maap-zw.instedd.org'
+    }
+  },
+  {
+    name: 'sz',
+    config: {
+      API_URL: 'https://maap-sz.instedd.org'
+    }
+  }
+];
