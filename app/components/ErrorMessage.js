1c8551b5b8c658c9c4161e2d48c8893ef549dda3
hdf1986 <hugo.farji+github@gmail.com>
2020-02-17 17:13:17 -0300

    Added validation for format

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -5,11 +5,18 @@ import React from 'react';
 import style from './ErrorMessage.scss';
 
 type Props = {
-  children: Node | string
+  children: Node | string,
+  center?: boolean
 };
 
-const ErrorMessage = ({ children }: Props) => (
-  <div className={style.errorMessage}>{children}</div>
+const ErrorMessage = ({ children, center }: Props) => (
+  <div className={[style.errorMessage, center ? style.center : ''].join(' ')}>
+    {children}
+  </div>
 );
+
+ErrorMessage.defaultProps = {
+  center: false
+};
 
 export default ErrorMessage;
