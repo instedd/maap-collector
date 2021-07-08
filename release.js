104756c4c996e4d271d6b6bdb7e660927bf7fb0d
Matías García Isaía <mgarcia@manas.tech>
2021-07-08 13:09:27 -0300

    Move CI to Github Actions (#331)
    * Add Github Actions to test project can build on pushes
    
    * Checkout code before trying to build in CI
    
    * Delete old TravisCI config
    
    So long, and thanks for all the fish!
    
    * Fix NodeJS version on CI builds
    
    * Run End-to-End tests in CI using xvfb-run
    
    * Build on CI on Windows/macOS as a matrix
    
    * Preview releasing instances' build from Github Actions
    
    I've commented out the country instances for now, just in case
    
    * Expose GITHUB_TOKEN for release CI script
    
    * Release 1.2.1.1 to test new CI builds
    
    * Release 1.2.2 to test new CI releases
    
    * Test builds for staging & demo only
    
    We're still drafting the workflow, so I don't want to wait for all the instances
    
    * Build macOS releases as .pkg
    
    macOS Catalina (the oldest macOS version supported by Github Actions)[0]
    is not supported by our current electron-builder version to build .dmg
    images[1]. Upgrading electron-builder seems to require upgrading NodeJS'
    version - and I don't want to get lost into that rabbit hole now.
    
    The workaround is to build .pkg versions for macOS distribution - that is
    supported by our current electron-build
    
    [0] https://github.com/actions/virtual-environments/blob/cd920950ecab4754858eb395a1a763ae20deb342/README.md
    [1] https://github.com/electron-userland/electron-builder/issues/3990#issuecomment-553734294
    
    * Run builds by platform to avoid creating multiple drafts
    
    The matrix key's order defines which job runs first[0], so this
    change will hopefully make all Windows builds ran before their
    macOS counter-parts, effectively avoiding the race condition
    between two `release.js` scripts listing the releases at the
    same time - and then both creating a draft each.
    
    It's not a hard guarantee, but if it may be a cheap workaround -
    and it's not that much of an issue if the workaround fail.
    
    [0] https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idstrategymatrix
    
    * Avoid repeating INSTANCES list in Github Actions
    
    This allows us to split release creation and installers upload in
    two separate jobs, so we avoid creating two different drafts for
    each release
    
    * Test if we need to manually create the Github Release
    
    `electron-builder` must be doing it by default
    
    * Avoid creating Release Drafts from CI script
    
    electron-builder takes care of that
    
    * Move Release job into a separate workflow
    
    That'll allow us to re-run that step independently.
    We'll then split it into a workflow per instance.
    
    * Rollback version change
    
    It was a temporal change to be able to debug the builds.
    
    This commit rolls it back so we can merge this branch.
    
    See a2f831cf4bcae2405b6aeef4a1f695156e232f3b

--- release.js
+++ release.js
@@ -1,73 +1,43 @@
 const { spawn } = require('child_process');
-const Octokit = require('@octokit/rest');
 const fs = require('fs');
 const { version } = require('./package.json');
 const environments = require('./environments');
 
-const { GITHUB_TOKEN } = process.env;
-const octokit = Octokit({
-  auth: GITHUB_TOKEN,
-  userAgent: 'Maap collector release tool'
-});
+if (!process.env.INSTANCE) {
+  return console.log('INSTANCE env var is missing');
+}
 
 console.log('Releasing Maap collector');
 console.log(`Version ${version}`);
 
 const versionNameTag = (versionNumber, name) => `${versionNumber}-${name}`;
-octokit.repos
-  .listReleases({
-    owner: 'instedd',
-    repo: 'maap-collector'
-  })
-  .then(async data => {
-    const { data: releases } = data;
-    const environment = environments.find(k => k.name === process.env.INSTANCE);
-    if (!process.env.INSTANCE)
-      return console.log('INSTANCE env var is missing');
-    const versionName = versionNameTag(version, environment.name);
-    if (!releases.map(({ name }) => name).includes(versionName)) {
-      octokit.repos.createRelease({
-        owner: 'instedd',
-        repo: 'maap-collector',
-        tag_name: `V${versionName}`,
-        draft: true,
-        name: versionName
-      });
-    }
-    console.log(`Releasing ${versionName} with config:`);
-    console.log(environment.config);
+const environment = environments.find(k => k.name === process.env.INSTANCE);
+const versionName = versionNameTag(version, environment.name);
+console.log(`Releasing ${versionName} with config:`);
+console.log(environment.config);
 
-    fs.writeFileSync(
-      './app/constants/config.override.json',
-      JSON.stringify(environment.config)
-    );
-    // eslint-disable-next-line
-    const packageJson = require('./package.json');
-    packageJson.version = versionName;
-    packageJson.name = `maap-collector-${process.env.INSTANCE}`;
-    packageJson.productName = `Maap Collector ${process.env.INSTANCE}`;
-    packageJson.productName = `Maap Collector ${process.env.INSTANCE}`;
-    packageJson.build.productName = `Maap Collector ${process.env.INSTANCE}`;
-    packageJson.build.appId = `org.develar.MaapCollector${process.env.INSTANCE.toUpperCase()}`;
-    fs.writeFileSync('./package.json', JSON.stringify(packageJson));
-    await new Promise(resolve => {
-      const ls = spawn('yarn', ['package-ci'], {
-        shell: process.platform === 'win32'
-      });
-      ls.stdout.on('data', output => {
-        console.log(`stdout: ${output.toString()}`);
-      });
-
-      ls.stderr.on('data', output => {
-        resolve(output);
-        console.log(`stderr: ${output.toString()}`);
-      });
-
-      ls.on('exit', output => {
-        resolve(output);
-        console.log(`child process exited with code ${output.toString()}`);
-      });
-    });
-    return data;
-  })
-  .catch(err => console.log(err));
+fs.writeFileSync(
+  './app/constants/config.override.json',
+  JSON.stringify(environment.config)
+);
+// eslint-disable-next-line
+const packageJson = require('./package.json');
+packageJson.version = versionName;
+packageJson.name = `maap-collector-${process.env.INSTANCE}`;
+packageJson.productName = `Maap Collector ${process.env.INSTANCE}`;
+packageJson.productName = `Maap Collector ${process.env.INSTANCE}`;
+packageJson.build.productName = `Maap Collector ${process.env.INSTANCE}`;
+packageJson.build.appId = `org.develar.MaapCollector${process.env.INSTANCE.toUpperCase()}`;
+fs.writeFileSync('./package.json', JSON.stringify(packageJson));
+const packageTask = spawn('yarn', ['package-ci'], {
+  shell: process.platform === 'win32'
+});
+packageTask.stdout.on('data', output => {
+  console.log(`stdout: ${output.toString()}`);
+});
+packageTask.stderr.on('data', output => {
+  console.log(`stderr: ${output.toString()}`);
+});
+packageTask.on('exit', output => {
+  console.log(`child process exited with code ${output.toString()}`);
+});
